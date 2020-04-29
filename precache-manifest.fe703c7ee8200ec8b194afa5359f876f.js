2def5aae5ed8cfe3d83971bb04f014f1e2d30087
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-04-29 06:46:03 +0000

    Updates

---
+++ precache-manifest.fe703c7ee8200ec8b194afa5359f876f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfe3c582528cb22e56e6a103a405519",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3182908a674dc1f899cf",
+    "url": "/monsters-rolodex/static/css/main.4341edda.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3182908a674dc1f899cf",
+    "url": "/monsters-rolodex/static/js/main.9e460515.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
