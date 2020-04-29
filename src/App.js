29ca21610116dbe9e86a30d7bf7b7767be43c1ca
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-04-29 06:14:44 +0000

    Feature: Adding a new component class to holde the monsters in a separate Card component, passing arguments properly

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      return response.json();
-    })
-    .then(users=>{
-      this.setState({
-        monsters: users
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({
+          monsters: users,
+        });
       });
-    });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
